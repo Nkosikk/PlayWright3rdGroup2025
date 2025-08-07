a0a54f21604d26c6fb8926a8046c53ea63e5a04c
Jabulane <jabulaneMtimkulu@yahoo.com>
2025-08-07 19:38:02 +0200

    Basic Browser Launch

--- src/test/java/LaunchBrowser.java
+++ src/test/java/LaunchBrowser.java
@@ -11,7 +11,16 @@ public class LaunchBrowser {
             Page page = context.newPage();
             page.navigate("https://www.saucedemo.com");
             page.fill("#user-name", "standard_user");
-            Thread.sleep(1000);
+            page.fill("#password","secret_sauce");
+            page.click("#login-button");
+
+            page.waitForLoadState();
+
+            page.waitForURL("https://www.saucedemo.com/inventory.html");
+
+            page.isVisible("#header_container > div.header_secondary_container > span");
+
+            Thread.sleep(3000);
 
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
