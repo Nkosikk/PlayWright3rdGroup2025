1b12f224562a0aa30e60b1a566ec2e431040532f
phelo <zwedalaphelo@gmail.com>
2025-08-07 19:38:49 +0200

    created login

--- src/test/java/LaunchBrowser.java
+++ src/test/java/LaunchBrowser.java
@@ -9,9 +9,17 @@ public class LaunchBrowser {
             Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
             BrowserContext context = browser.newContext();
             Page page = context.newPage();
+            // Navigate to the SauceDemo login page and perform login
             page.navigate("https://www.saucedemo.com");
             page.fill("#user-name", "standard_user");
             Thread.sleep(1000);
+            page.fill("#password", "secret_sauce");
+            Thread.sleep(1000);
+            // Click the login button
+            page.click("#login-button");
+            // verify user is logged in by checking for word "Products"
+            page.waitForSelector("text=Products");
+
 
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
