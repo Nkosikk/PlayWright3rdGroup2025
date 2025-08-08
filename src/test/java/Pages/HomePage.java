4c7744be55debba56982b77dcd02480251abcc8e
tlhap003 <Phuti.Tlhako@vcontractor.co.za>
2025-08-08 20:38:22 +0200

    Wrote a Method to confirm Practice Assessment Page

--- src/test/java/Pages/HomePage.java
+++ src/test/java/Pages/HomePage.java
@@ -7,6 +7,7 @@ public class HomePage {
 
     private String welcomeText = "h2:has-text('Welcome to Ndosi Online Automation Boot-Camp')";
     private String learningMaterialsLocator = "button:has-text('Learning Materials')";
+    private String PracticeAssessments = "h2:has-text('Practice Assessments')";
 
     public HomePage(Page page){
         this.page = page;
@@ -20,6 +21,18 @@ public class HomePage {
     public HomePage clickLearningMaterials() {
         page.click(learningMaterialsLocator);
         return new HomePage(page);
+    }
+        //User confirm PRACTICE Assessment Page
+        public boolean isPracticeAssessmentsTextVisible() {
+        return page.isVisible(PracticeAssessmentsText);
+
+        }
+
+
+        //User select Selenium Practice Button
+        public HomePage clickSeleniumPractice() {
+            await page.getByRole('button', { name: 'Selenium Practice' }).click();
+            return new HomePage(page);
 
     }
 
