09928a7ef5a0c2ed97e13278f2aa24c5a70279eb
lence <lmlence@gmail.com>
2025-08-08 16:42:59 +0200

    added homepage methods

---
+++ src/test/java/Pages/HomePage.java
@@ -0,0 +1,29 @@
+package Pages;
+
+import com.microsoft.playwright.Page;
+
+public class HomePage {
+    Page page;
+
+    private String welcomeText = "h2:has-text('Welcome to Ndosi Online Automation Boot-Camp')";
+    private String learningMaterialsLocator = "button:has-text('Learning Materials')";
+
+    public HomePage(Page page) {
+        this.page = page;
+    }
+
+    public boolean isWelcomeTextVisible() {
+        return page.isVisible(welcomeText);
+    }
+
+    public boolean isLearningMaterialsButtonVisible() {
+        return page.isVisible(learningMaterialsLocator);
+    }
+
+    public boolean isPageLoaded() {
+        return isWelcomeTextVisible() && isLearningMaterialsButtonVisible();
+    }
+
+
+
+}
