4a4b635bded2672269ff24085de5033a3ab4f62d
mnyinyi <mnyinyi20@gmail.com>
2025-08-09 18:46:13 +0200

    chage home page file

--- src/test/java/Pages/HomePage.java
+++ src/test/java/Pages/HomePage.java
@@ -1,20 +1,30 @@
 package Pages;
 
 import com.microsoft.playwright.Page;
+
 import org.testng.annotations.Test;
 
 @Test
 public class HomePage {
 
-    Page page;
+    // A method to set the page after instantiation
+     private  Page page;
 
     private String welcomeText = "h2:has-text('Welcome to Ndosi Online Automation Boot-Camp')";
     private String learningMaterialsLocator = "button:has-text('Learning Materials')";
     private  String sleniumPractiseLocator = "button:has-text('Slenium Practise')";
 
+
+    public HomePage() {
+    }
+
     public HomePage(Page page){
         this.page = page;
     }
+
+
+
+    //This is the public no-argument constructor TestNG needs
 
     public boolean isLoggedIn() {
         return page.isVisible("button:has-text('Logout')");
