6afad7b20e5ed976f9a719c4f6ff7ea25d2b980d
mnyinyi <mnyinyi20@gmail.com>
2025-08-09 17:14:28 +0200

    Creating metho to click on selenium practise

--- src/test/java/Pages/HomePage.java
+++ src/test/java/Pages/HomePage.java
@@ -1,12 +1,16 @@
 package Pages;
 
 import com.microsoft.playwright.Page;
+import org.testng.annotations.Test;
 
+@Test
 public class HomePage {
+
     Page page;
 
     private String welcomeText = "h2:has-text('Welcome to Ndosi Online Automation Boot-Camp')";
     private String learningMaterialsLocator = "button:has-text('Learning Materials')";
+    private  String sleniumPractiseLocator = "button:has-text('Slenium Practise')";
 
     public HomePage(Page page){
         this.page = page;
@@ -19,6 +23,11 @@ public class HomePage {
 
     public HomePage clickLearningMaterials() {
         page.click(learningMaterialsLocator);
+        return new HomePage(page);
+
+    }
+    public HomePage clickSleniumPractise() {
+        page.click(sleniumPractiseLocator);
         return new HomePage(page);
 
     }
