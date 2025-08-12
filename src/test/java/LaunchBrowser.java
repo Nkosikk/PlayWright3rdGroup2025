fd4027a7f03ac413980cd49e38223429678fd0ea
phelo <zwedalaphelo@gmail.com>
2025-08-12 14:41:29 +0200

    created login

--- src/test/java/LaunchBrowser.java
+++ src/test/java/LaunchBrowser.java
@@ -10,8 +10,25 @@ public class LaunchBrowser {
             BrowserContext context = browser.newContext();
             Page page = context.newPage();
             page.navigate("https://www.saucedemo.com");
+            //Enter Credentials
             page.fill("#user-name", "standard_user");
             Thread.sleep(1000);
+            page.fill("#password", "secret_sauce");
+            Thread.sleep(1000);
+            //Click on Login Button
+            page.click("#login-button");
+            Thread.sleep(5000); // Wait for 5 seconds to see the result
+            //validate login
+            String title = page.title();
+            if (title.equals("Products")) {
+                System.out.println("Login successful!");
+            } else {
+                System.out.println("Login failed!");
+            }
+
+
+
+
 
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
