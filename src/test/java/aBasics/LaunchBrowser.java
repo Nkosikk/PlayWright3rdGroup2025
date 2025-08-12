71d4a9422be8ff1a027ea51c58aa38aa5879dbb4
Mr Wilson <refilwe.wilson@gmail.com>
2025-08-12 07:07:29 +0200

    I have created the the steps of making the selecting the buttons and storing the data into the text box

--- src/test/java/aBasics/LaunchBrowser.java
+++ src/test/java/aBasics/LaunchBrowser.java
@@ -11,9 +11,19 @@ public class LaunchBrowser {
             Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
             BrowserContext context = browser.newContext();
             Page page = context.newPage();
-            page.navigate("https://www.saucedemo.com");
-            page.fill("#user-name", "standard_user");
-            Thread.sleep(1000);
+            page.navigate("https://www.ndosiautomation.co.za");
+            page.click("button:has-text('Learning Materials')");
+            Thread.sleep(8000);
+            page.click("button:has-text('Selenium Practice')");
+            Thread.sleep(8000);
+            page.fill("#username", "Refilwe");
+            Thread.sleep(8000);
+            page.fill("#password", "Refilwe123");
+            Thread.sleep(8000);
+            page.click("button:has-text('Login')");
+            Thread.sleep(8000);
+            //page.fill("#user-name", "standard_user");
+           // Thread.sleep(1000);
 
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
