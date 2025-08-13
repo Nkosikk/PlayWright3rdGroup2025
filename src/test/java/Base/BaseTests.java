a400fb6463f9bd04ffbef701967d6a7b8736adb6
lence <lmlence@gmail.com>
2025-08-13 20:08:59 +0200

    added BaseTest class

---
+++ src/test/java/Base/BaseTests.java
@@ -0,0 +1,26 @@
+package Base;
+
+import BrowserFactory.PlayWrightBrowserFactory;
+import Pages.HomePage;
+import com.microsoft.playwright.Page;
+import org.testng.annotations.BeforeTest;
+
+import java.util.Properties;
+
+public class BaseTests {
+
+    protected Page page;
+    protected HomePage homePage;
+    PlayWrightBrowserFactory pf;
+    Properties prop;
+
+    @BeforeTest
+    public void setup(){
+        pf = new PlayWrightBrowserFactory();
+        prop = pf.init_prop(); // Initialize properties if needed
+        pf.initBrowser(prop);
+        page = pf.getPage();
+        homePage = new HomePage(page);
+    }
+
+}
