2a3d95797280b600cfa49213f89dc2e8e2b4092c
Khulekani <ktrumashasha@gmail.com>
2025-08-17 14:57:27 +0200

    added ExtentReportListener class

--- src/test/java/ExtentReport/ExtentReportListener.java
+++ src/test/java/ExtentReport/ExtentReportListener.java
@@ -1,4 +1,47 @@
 package ExtentReport;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
 public class ExtentReportListener {
+    private static ExtentReports extent;
+    private static ThreadLocal<ExtentTest> test = new ThreadLocal<>();
+
+    @Override
+    public void onStart(ITestContext context) {
+        ExtentSparkReporter spark = new ExtentSparkReporter("extent-report.html");
+        extent = new ExtentReports();
+        extent.attachReporter(spark);
+    }
+
+    @Override
+    public void onTestStart(ITestResult result) {
+        ExtentTest newTest = extent.createTest(result.getMethod().getMethodName());
+        test.set(newTest);
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult result) {
+        test.get().pass("Test passed");
+    }
+
+    @Override
+    public void onTestFailure(ITestResult result) {
+        test.get().fail("Test failed");
+        // Add code to take and attach a screenshot here
+    }
+
+    @Override
+    public void onTestSkipped(ITestResult result) {
+        test.get().skip("Test skipped");
+    }
+
+    @Override
+    public void onFinish(ITestContext context) {
+        extent.flush();
+    }
 }
