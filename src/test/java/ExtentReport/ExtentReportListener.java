b66015940bee01a07425755b8d9789a67be8d131
Khulekani <ktrumashasha@gmail.com>
2025-08-17 15:01:21 +0200

    added on runner  ExtentReportListener

--- src/test/java/ExtentReport/ExtentReportListener.java
+++ src/test/java/ExtentReport/ExtentReportListener.java
@@ -7,41 +7,41 @@ import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
-public class ExtentReportListener {
-    private static ExtentReports extent;
-    private static ThreadLocal<ExtentTest> test = new ThreadLocal<>();
+public class ExtentReportListener implements ITestListener {
+        private static ExtentReports extent;
+        private static ThreadLocal<ExtentTest> test = new ThreadLocal<>();
 
-    @Override
-    public void onStart(ITestContext context) {
-        ExtentSparkReporter spark = new ExtentSparkReporter("extent-report.html");
-        extent = new ExtentReports();
-        extent.attachReporter(spark);
-    }
+        @Override
+        public void onStart(ITestContext context) {
+            ExtentSparkReporter spark = new ExtentSparkReporter("extent-report.html");
+            extent = new ExtentReports();
+            extent.attachReporter(spark);
+        }
 
-    @Override
-    public void onTestStart(ITestResult result) {
-        ExtentTest newTest = extent.createTest(result.getMethod().getMethodName());
-        test.set(newTest);
-    }
+        @Override
+        public void onTestStart(ITestResult result) {
+            ExtentTest newTest = extent.createTest(result.getMethod().getMethodName());
+            test.set(newTest);
+        }
 
-    @Override
-    public void onTestSuccess(ITestResult result) {
-        test.get().pass("Test passed");
-    }
+        @Override
+        public void onTestSuccess(ITestResult result) {
+            test.get().pass("Test passed");
+        }
 
-    @Override
-    public void onTestFailure(ITestResult result) {
-        test.get().fail("Test failed");
-        // Add code to take and attach a screenshot here
-    }
+        @Override
+        public void onTestFailure(ITestResult result) {
+            test.get().fail("Test failed");
+            // Add code to take and attach a screenshot here
+        }
 
-    @Override
-    public void onTestSkipped(ITestResult result) {
-        test.get().skip("Test skipped");
-    }
+        @Override
+        public void onTestSkipped(ITestResult result) {
+            test.get().skip("Test skipped");
+        }
 
-    @Override
-    public void onFinish(ITestContext context) {
-        extent.flush();
+        @Override
+        public void onFinish(ITestContext context) {
+            extent.flush();
+        }
     }
-}
