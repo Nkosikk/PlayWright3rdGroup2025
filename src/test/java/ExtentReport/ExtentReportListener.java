806f605fb00b03e34eb73802f0a97aaa5fadcd25
mnyinyi <mnyinyi20@gmail.com>
2025-08-19 19:34:38 +0200

    Adding extent Report

--- src/test/java/ExtentReport/ExtentReportListener.java
+++ src/test/java/ExtentReport/ExtentReportListener.java
@@ -1,4 +1,27 @@
 package ExtentReport;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+import java.io.File;
+
 public class ExtentReportListener {
+    private static String reportDir = System.getProperty("user.dir") + "/Reports/ndosiOnlineAutomationBootCamp.html";
+
+    private static ExtentReports extentReports;
+    private static ExtentSparkReporter extentSparkReporter;
+
+    public static ExtentReports extentSetup() {
+
+        extentReports = new ExtentReports();
+        extentSparkReporter = new ExtentSparkReporter(new File(reportDir));
+        extentReports.attachReporter(extentSparkReporter);
+
+        extentSparkReporter.config().setDocumentTitle("Extent Report");
+        extentSparkReporter.config().setReportName("ndosi OnlineAutomation BootCamp");
+
+        extentReports.setSystemInfo("OS", System.getProperty("os.name"));
+        extentReports.setSystemInfo("Exection Machine", System.getProperty("user.name"));
+        return extentReports;
+    }
 }
