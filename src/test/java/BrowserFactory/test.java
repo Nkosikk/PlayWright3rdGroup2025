1f0c3d05d74ee026c441550bfda2d43382c3c685
Amanda <simangeleramavhale@gmail.com>
2025-08-20 21:04:47 +0200

    Added code on browserfactory class to initilise browser

--- src/test/java/BrowserFactory/test.java
+++ src/test/java/BrowserFactory/test.java
@@ -1,4 +1,47 @@
 package BrowserFactory;
 
+import com.microsoft.playwright.*;
+
 public class test {
+    Playwright playwright;
+    Browser browser;
+    BrowserContext browserContext;
+    Page page;
+
+    public Page initBrowser(String browserName) {
+        System.out.println("The browser name: " + browserName);
+
+            playwright = Playwright.create();
+            switch (browserName.toLowerCase()) {
+                case "chromium":
+                  browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+                    break;
+                case "firefox":
+                    browser = playwright.firefox().launch(new BrowserType.LaunchOptions().setHeadless(false));
+                    break;
+                case "webkit":
+                    browser = playwright.webkit().launch(new BrowserType.LaunchOptions().setHeadless(false));
+                    break;
+                case "chrome":
+                    browser = playwright.chromium().launch(
+                            new BrowserType.LaunchOptions()
+                                    .setChannel("chrome")
+                                    .setHeadless(false)
+                    );
+                    break;
+                default:
+                    throw new IllegalArgumentException("Unsupported browser: " + browserName);
+
+            }
+        browserContext = browser.newContext();
+        page = browserContext.newPage();
+        // Navigate to a default page
+        page.navigate("https://www.example.com");
+        return page;
+
+    }
+
+
+
 }
+
